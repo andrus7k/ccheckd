0df565407a5d10eeaa488751f842ae4ec9fb2ccb
Andrus Viik <andrus@a7k.pri.ee>
2012-03-25 15:29:34 +0300

    moved starter script out of src to toplevel

--- src/ccheckd.py
+++ src/ccheckd.py
@@ -4,19 +4,21 @@ __date__ ="$Mar 24, 2012 2:52:57 PM$"
 
 from collectd import Collectd
 
-if __name__ == '__main__':
-    c = Collectd('/var/run/collectd.sock', noisy=True)
+class CCheckD():
 
-    for val in c.listval():
-        stamp, identifier = val.split(' ',1)
-        host, plugin, type = identifier.split('/',4)
-        typeinstance = ''
-        plugininstance = ''
+    def main(self):
+        c = Collectd('/var/run/collectd.sock', noisy=True)
 
-        if "-" in plugin:
-            plugin, plugininstance = plugin.split('-',1)
+        for val in c.listval():
+            stamp, identifier = val.split(' ',1)
+            host, plugin, type = identifier.split('/',4)
+            typeinstance = ''
+            plugininstance = ''
 
-        if "-" in type:
-            type, typeinstance = type.split('-',1)
+            if "-" in plugin:
+                plugin, plugininstance = plugin.split('-',1)
 
-        print "%s\t%s\t%s\t%s\t%s\t%s" % (stamp, host, plugin, plugininstance, type, typeinstance)
+            if "-" in type:
+                type, typeinstance = type.split('-',1)
+
+            print "%s\t%s\t%s\t%s\t%s\t%s" % (stamp, host, plugin, plugininstance, type, typeinstance)
